e01b04ce4fc438e424b71e46816842685e57802c
John <johnshew@twitter.com>
2015-09-08 15:55:31 -0700

    Added converter and modified visibility.

---
+++ Simple MVVM UWP with CRUD/Converters/ObjectExistsToVisible.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.UI.Xaml.Data;
+
+namespace Converters
+{
+    public class ObjectExistsToVisible : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string culture)
+        {
+            return (value == null) ? Windows.UI.Xaml.Visibility.Collapsed
+                : Windows.UI.Xaml.Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, string culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
