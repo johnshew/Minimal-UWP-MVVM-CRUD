d61f74ba42b40362f5a06ffff8a84247cc0ab232
John <johnshew@twitter.com>
2015-09-08 14:49:46 -0700

    Added the Data layer.

---
+++ Simple MVVM UWP with CRUD/Data/FakeService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+namespace Data
+{
+    public class Person
+    {
+        public String Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class FakeService
+    {
+        public static String Name = "Fake Data Service.";
+
+        public static List<Person> GetPeople()
+        {
+            Debug.WriteLine("GET for people.");
+            return new List<Person>()
+                {
+                    new Person() { Name="Chris Cole", Age=10 },
+                    new Person() { Name="Kelly Kale", Age=32 },
+                    new Person() { Name="Dylan Durbin", Age=18 }
+                };
+        }
+
+        public static void Write(Person person)
+        {
+            Debug.WriteLine("INSERT person with name " + person.Name);
+        }
+
+        public static void Delete(Person person)
+        {
+            Debug.WriteLine("DELETE person with name " + person.Name);
+        }
+    }
+}
