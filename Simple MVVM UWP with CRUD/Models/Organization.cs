afa9f3cb09ede4325b3d7ac70a7f9ccb808a5900
John <johnshew@twitter.com>
2015-09-08 14:54:59 -0700

    Added Models.

---
+++ Simple MVVM UWP with CRUD/Models/Organization.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Data;
+
+namespace Models
+{
+    public class Organization
+    {
+        public List<Person> People { get; set; }
+        public String Name { get; set; }
+
+        public Organization(String databaseName)
+        {
+            Name = databaseName;
+            People = FakeService.GetPeople();
+        }
+
+        public void Add(Person person)
+        {
+            if (!People.Contains(person))
+            {
+                People.Add(person);
+                FakeService.Write(person);
+            }
+        }
+
+        public void Delete(Person person)
+        {
+            if (People.Contains(person))
+            {
+                People.Remove(person);
+                FakeService.Delete(person);
+            }
+        }
+
+        public void Update(Person person)
+        {
+            FakeService.Write(person);
+        }
+    }
+}
